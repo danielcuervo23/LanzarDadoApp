328e9b8387385a89bf5856dcd05ee78f34c2eecf
danielcuervo23 <juanda232310@gmail.com>
2026-09-23 09:32:35 -0500

    Dado aleatorio

--- app/src/main/java/me/juandanielcuervovilla/statecompose/MainActivity.kt
+++ app/src/main/java/me/juandanielcuervovilla/statecompose/MainActivity.kt
@@ -3,23 +3,72 @@ package me.juandanielcuervovilla.statecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import me.juandanielcuervovilla.statecompose.ui.theme.StateComposeTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
 
-            StateScreen()
+        setContent {
+            DiceApp()
+        }
+    }
+}
+
+@androidx.compose.runtime.Composable
+fun DiceApp() {
+
+    var number by remember { mutableIntStateOf(1) }
+
+    val diceImage = when (number) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Image(
+            painter = painterResource(id = diceImage),
+            contentDescription = "Dado",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(
+            modifier = Modifier.height(30.dp)
+        )
+
+        Button(
+            onClick = {
+                number = (1..6).random()
+            }
+        ) {
+            Text("Lanzar el dado!!!")
         }
     }
 }
